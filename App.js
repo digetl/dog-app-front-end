dad10918decf4975a05c26d2124aff32900b5b9a
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-07-11 16:23:43 +0100

    Created basic flexible

--- App.js
+++ App.js
@@ -4,8 +4,53 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+
+
+        <View style={styles.wideBox}>
+          <View style={styles.wideButton}>
+            <Text style={styles.buttonText}>Find your next best friend</Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}>Articles</Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}>Profile</Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}></Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}></Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}>Box 1</Text>
+          </View>
+        </View>
+
+        <View style={styles.box1}>
+          <View style={styles.button1}>
+            <Text style={styles.buttonText}>Inbox</Text>
+          </View>
+        </View>
+
+
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +60,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingTop: 40,
     justifyContent: 'center',
   },
+  box1: {
+    backgroundColor: '#699bc6',
+    marginVertical: 10, 
+    alignItems: 'center',
+    margin: 10,
+
+  },
+  button1: {
+    minWidth: 150,
+    height: 100,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  wideBox: {
+    width: '100%',
+  },
+  wideButton: {
+    backgroundColor: '#699bc6',
+    margin: 10,
+    height: 100,
+    justifyContent: 'center',
+  }
+ 
 });
