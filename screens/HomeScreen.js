725ce5d4a97111b08131583262a57890a0d4cedc
Victor <victorsetlhare@gmail.com>
2020-07-14 20:43:06 +0100

    title

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,22 +14,40 @@
 // export default HomeScreen;
 
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text>NEXT BEST FRIEND</Text>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>NEXT BEST FRIEND</Text>
+        </View>
+        <View>
+          <Text style={styles.subtitle}>@who let the dogs out</Text>
+        </View>
       </View>
     );
   }
 }
-const styles = {
+const styles = StyleSheet.create({
   wrapper: {
     backgroundColor: "#c14a30",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-};
+  title: {
+    color: "white",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "200",
+  },
+  titleWrapper: {
+    justifyContent: "center",
+    flex: 1,
+  },
+});
