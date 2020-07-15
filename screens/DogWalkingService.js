66c5e5bc363f3e25dd6106fe5c12df8bc3800ed3
cjscrimmy <craigscrimgeour@yahoo.com>
2020-07-15 17:15:52 +0100

    trying to get navigation working :(

---
+++ screens/DogWalkingService.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+import WideBox from "../styled-components/WideBox";
+
+import Footer from ".././components/Footer";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity} from "react-native";
+
+import SingleService from "../localServices/SingleService";
+import { ScrollView } from "react-native-gesture-handler";
+
+const DATA = [
+  {
+    id: "001",
+    service_type: "Dog Walker",
+    name: "The Dashing White Rabbit Company",
+    address: "3 Sinclair Place, Edinburgh, EH11 1AG",
+    phone_number: "0131 478 5098"
+    
+  },
+  {
+    id: "002",
+    service_type: "Dog Walker",
+    name: "Healthy Hounds",
+    address: "7/3 New Orchardfield, Edinburgh, EH6 5ES",
+    phone_number: "07837 774478"
+  },
+  {
+    id: "003",
+    service_type: "Dog Walker",
+    name: "P and J Pet Care",
+    address: "Gilmour Street, Edinburgh, EH8 9HR",
+    phone_number: "07999 482440"
+  },
+  {
+    id: "004",
+    service_type: "Dog Walker",
+    name: "Dog Walking Edinburgh",
+    address: "14/7 Murano Place, Edinburgh, EH7 5HG",
+    phone_number: "07814 447450"
+  },
+];
+
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text>{item.name}</Text>
+  </TouchableOpacity>
+);
+
+const DogWalkingService = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#52a6cb" : "#a4c1db";
+
+    return (
+      
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+
+        style={{ backgroundColor }}
+      />
+    );
+  };
+
+  return (
+    <>
+      <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <WideBox>
+          <Text>Local Services</Text>
+        </WideBox>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+        />
+        <SingleService service={DATA.find((item) => item.id === selectedId)}></SingleService>
+
+      </SafeAreaView>
+    </ScrollView>
+    <Footer></Footer>
+  </> 
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    justifyContent: "space-around",
+    margin: 15,
+  },
+  item: {
+    padding: 10,
+    marginVertical: 4,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  topTitle: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
+
+
+export default DogWalkingService;
--- screens/LocalServices.js
+++ screens/LocalServices.js
@@ -1,113 +1,99 @@
-import React, { useState } from "react";
+import React from "react";
 
+import { ScrollView } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
-import WideBox from "../styled-components/WideBox";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+  } from "react-native";
 
 import Footer from ".././components/Footer";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity} from "react-native";
 
-import SingleService from "../localServices/SingleService";
-import { ScrollView } from "react-native-gesture-handler";
+const LocalServices = () => {
 
-const DATA = [
-  {
-    id: "001",
-    service_type: "Dog Walker",
-    name: "The Dashing White Rabbit Company",
-    address: "3 Sinclair Place, Edinburgh, EH11 1AG",
-    phone_number: "0131 478 5098"
-    
-  },
-  {
-    id: "002",
-    service_type: "Dog Walker",
-    name: "Healthy Hounds",
-    address: "7/3 New Orchardfield, Edinburgh, EH6 5ES",
-    phone_number: "07837 774478"
-  },
-  {
-    id: "003",
-    service_type: "Dog Walker",
-    name: "P and J Pet Care",
-    address: "Gilmour Street, Edinburgh, EH8 9HR",
-    phone_number: "07999 482440"
-  },
-  {
-    id: "004",
-    service_type: "Dog Walker",
-    name: "Dog Walking Edinburgh",
-    address: "14/7 Murano Place, Edinburgh, EH7 5HG",
-    phone_number: "07814 447450"
-  },
-];
-
-const Item = ({ item, onPress, style }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-    <Text style={styles.title}>{item.service_type} - {item.name}</Text>
-  </TouchableOpacity>
-);
-
-const Services = () => {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#52a6cb" : "#a4c1db";
-
+    const navigation = useNavigation();
+  
     return (
-      
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-
-        style={{ backgroundColor }}
-      />
+      <>
+        <ScrollView>
+            <TouchableOpacity onPress={() => navigation.navigate("DogWalkingService")}>
+                    <View style={styles.wideButton}>
+                        <Text style={styles.buttonText}>Dog Walking Service</Text>
+                    </View>
+            </TouchableOpacity>
+        </ScrollView>
+  
+        <Footer></Footer>
+      </>
     );
   };
+  
+  const styles = StyleSheet.create({
+    smallIconContainer: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+    },
+    image: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center",
+    },
+    leftBox: {
+      backgroundColor: "#e3924e",
+      alignItems: "center",
+      margin: 0,
+      width: "50%",
+    },
+    rightBox: {
+      backgroundColor: "#a4c1db",
+      alignItems: "center",
+      margin: 0,
+      width: "50%",
+    },
+    buttonText: {
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    button1: {
+      width: "100%",
+      height: 100,
+      backgroundColor: "#e3924e",
+      justifyContent: "center",
+    },
+    
+    orangeButton: {
+      backgroundColor: "#e3924e",
+    },
+    
+    greyButton: {
+      backgroundColor: "#f5f1f0",
+    },
+  
+    darkTanButton: {
+      backgroundColor: "#973618",
+    },
+    blueButton: {
+      backgroundColor: "#52a6cb",
+    },
+    mintButton: {
+      backgroundColor: "#19A79C",
+    },
+    button2: {
+      width: "100%",
+      height: 100,
+      backgroundColor: "#a4c1db",
+      justifyContent: "center",
+    },
+  });
+  
 
-  return (
-    <>
-      <ScrollView>
-      <SafeAreaView style={styles.container}>
-        <WideBox>
-          <Text style={styles.topTitle}>Local Services</Text>
-        </WideBox>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          extraData={selectedId}
-        />
-        <SingleService service={DATA.find((item) => item.id === selectedId)}></SingleService>
-
-      </SafeAreaView>
-    </ScrollView>
-    <Footer></Footer>
-  </> 
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-    justifyContent: "space-around",
-    margin: 15,
-  },
-  item: {
-    padding: 10,
-    marginVertical: 4,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 16,
-    textAlign: "center",
-  },
-  topTitle: {
-    fontSize: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-});
-
-
-export default Services;
+export default LocalServices;
