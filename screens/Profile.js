c12cdeb2bb5f3e96711172834346739d3fbcf0b5
Victor <victorsetlhare@gmail.com>
2020-07-16 10:54:53 +0100

    View

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,19 +1,29 @@
-import React, from "react";
+import React from "react";
 import Footer from ".././components/Footer";
 
-import { FlatList, SafeAreaView, StatusBar, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
 import { ScrollView } from "react-native-gesture-handler";
 
-const Profile = () => {
+const Profile = ({ navigation }) => {
   return (
     <>
       <ScrollView>
-        <WideBox>
+        <TouchableOpacity style={(styles.buttonRow, styles.greenrow)}>
+          <View>
             <Text style={styles.title}>Profile Page </Text>
-        </WideBox>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
-      
+
       <Footer></Footer>
     </>
   );
