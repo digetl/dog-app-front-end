90f8b11c919d40b7a8cc5cbdc521a92319300ca2
Victor <victorsetlhare@gmail.com>
2020-07-16 11:02:43 +0100

    Styling

--- screens/Profile.js
+++ screens/Profile.js
@@ -44,6 +44,69 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
   },
+  leftBox: {
+    backgroundColor: "#e3924e",
+    alignItems: "center",
+    margin: 0,
+    width: "50%",
+  },
+  rightBox: {
+    backgroundColor: "#e3924e",
+    alignItems: "center",
+    margin: 0,
+    width: "50%",
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  button1: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#e3924e",
+    justifyContent: "center",
+  },
+
+  orangeButton: {
+    backgroundColor: "#e3924e",
+  },
+
+  greyButton: {
+    backgroundColor: "#f5f1f0",
+  },
+
+  darkTanButton: {
+    backgroundColor: "#973618",
+  },
+  blueButton: {
+    backgroundColor: "#52a6cb",
+  },
+  mintButton: {
+    backgroundColor: "#19A79C",
+  },
+  button2: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#a4c1db",
+    justifyContent: "center",
+  },
+  buttonRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    paddingTop: 20,
+    paddingBottom: 20,
+    textAlign: "center",
+  },
+  greenrow: {
+    backgroundColor: "#326d67",
+  },
 });
 
 export default Profile;
